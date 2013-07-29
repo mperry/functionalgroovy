135a1c8c6186026223783b46efea97802fbd8400
Mark Perry <maperry78@yahoo.com.au>
2013-07-29 17:59:49 +1000

    Added lifting Mx through Option

--- core/src/main/groovy/com/github/mperry/fg/OptionStaticExtension.groovy
+++ core/src/main/groovy/com/github/mperry/fg/OptionStaticExtension.groovy
@@ -1,5 +1,8 @@
 package com.github.mperry.fg
 
+import fj.F
+import fj.F2
+import fj.F3
 import fj.data.Option
 import groovy.transform.TypeChecked
 
@@ -16,5 +19,41 @@ class OptionStaticExtension {
 	public static <A> Option<A> unit(Option option, A value) {
 		Option.<A>some(value)
 	}
+
+//	@TypeChecked
+	public static <A, B> F<Option<A>, Option<B>> liftM1(Option o, F<A, B> f) {
+		{ Option<A> oa, Option<B> ob ->
+			oa.map({ A a ->
+				f.f(a)
+			} as F)
+		} as F
+	}
+
+//	@TypeChecked
+	public static <A, B, C> F2<Option<A>, Option<B>, Option<C>> liftM2(Option o, F2<A, B, C> f) {
+		{
+			Option<A> oa, Option<B> ob ->
+				oa.bind({ A a ->
+					ob.map({ B b ->
+						f.f(a, b)
+					} as F)
+				} as F)
+		} as F2
+	}
+
+	@TypeChecked
+	public static <A, B, C, D> F3<Option<A>, Option<B>, Option<C>, Option<D>> liftM3(Option o, F3<A, B, C, D> f) {
+		{
+			Option<A> oa, Option<B> ob, Option<C> oc ->
+				oa.bind({ A a ->
+					ob.bind({ B b ->
+						oc.map({ C c ->
+							f.f(a, b, c)
+						} as F)
+					} as F)
+				} as F)
+		} as F3
+	}
+
 
 }

da970da18fd61d115857e6dcbaa3fc8ad6aec9a5
Mark Perry <maperry78@yahoo.com.au>
2013-11-08 10:12:56 +1000

    Moved demo dir to demo3 to prepare for demo module

--- demo3/src/test/groovy/com/github/mperry/fg/test/Demo.groovy
+++ demo3/src/test/groovy/com/github/mperry/fg/test/Demo.groovy
@@ -4,10 +4,12 @@ import org.junit.Test
 import fj.data.Option
 //import com.github.mperry.fg.Option
 
-@GrabResolver('https://raw.github.com/mperry/functionalgroovy/master/releases/')
-@Grab('org.functionalgroovy:functionalgroovy-main:0.1.1-SNAPSHOT')
+@GrabResolver('https://oss.sonatype.org/content/groups/public')
+//@GrabResolver('https://raw.github.com/mperry/functionalgroovy/master/releases/')
+//@Grab('org.functionalgroovy:functionalgroovy-main:0.1.1-SNAPSHOT')
+@Grab('com.github.mperry:functionalgroovy-core:0.2-SNAPSHOT')
 // next two are temporary until maven artifact reference is corrected
-@Grab('org.functionalgroovy:functionalgroovy-core:0.1.1-SNAPSHOT')
+//@Grab('org.functionalgroovy:functionalgroovy-core:0.1.1-SNAPSHOT')
 @GrabExclude('functionalgroovy:core')
 class Demo extends GroovyTestCase {
 
--- demo3/src/test/groovy/com/github/mperry/fg/test/Demo2.groovy
+++ demo3/src/test/groovy/com/github/mperry/fg/test/Demo2.groovy
@@ -9,10 +9,12 @@ import org.junit.Test
  * Time: 9:51 PM
  * To change this template use File | Settings | File Templates.
  */
-@GrabResolver('https://raw.github.com/mperry/functionalgroovy/master/releases/')
-@Grab('org.functionalgroovy:functionalgroovy-main:0.1.1-SNAPSHOT')
+@GrabResolver('https://oss.sonatype.org/content/groups/public')
+//@GrabResolver('https://raw.github.com/mperry/functionalgroovy/master/releases/')
+@Grab('com.github.mperry:functionalgroovy-core:0.2-SNAPSHOT')
+//@Grab('org.functionalgroovy:functionalgroovy-main:0.1.1-SNAPSHOT')
 // next two are temporary until maven artifact reference is corrected
-@Grab('org.functionalgroovy:functionalgroovy-core:0.1.1-SNAPSHOT')
+//@Grab('org.functionalgroovy:functionalgroovy-core:0.1.1-SNAPSHOT')
 @GrabExclude('functionalgroovy:core')
 class Demo2 extends GroovyTestCase {
 

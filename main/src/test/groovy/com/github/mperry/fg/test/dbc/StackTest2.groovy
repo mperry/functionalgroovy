1de4d24b14e4c6f586de829a0725db7963553301
Mark Perry <mark.perry@cua.com.au>
2013-12-03 10:08:43 +1000

    Run both recurisve and imperative generator

--- main/src/test/groovy/com/github/mperry/fg/test/dbc/StackTest2.groovy
+++ main/src/test/groovy/com/github/mperry/fg/test/dbc/StackTest2.groovy
@@ -2,7 +2,6 @@ package com.github.mperry.fg.test.dbc
 
 import com.github.mperry.fg.test.PropertyConfig
 import fj.F
-import fj.P1
 import fj.test.Arbitrary
 import fj.test.Gen
 import groovy.transform.TypeChecked
@@ -27,15 +26,15 @@ class StackTest2 {
         new ExceptionFreeStack<Integer>()
     }
 
-    Gen<Integer> genInt() {
+    Gen<Integer> genStackSize() {
         Gen.oneOf([Gen.value(0), Gen.value(1), Gen.choose(2, 10)].toFJList())
     }
 
     Gen<ExceptionFreeStack<Integer>> genStackImperative() {
-        genInt().map({ Integer n ->
+        genStackSize().map({ Integer n ->
             def s = empty()
             def r = new Random()
-            println "Creating stack of size $n"
+//            println "Creating stack of size $n"
             for (int i = 0; i < n; i++) {
                 def val = r.nextInt()
                 s.push(val)
@@ -61,22 +60,19 @@ class StackTest2 {
         Gen.oneOf([genEmpty(), genNonEmpty()].toFJList())
     }
 
-    Arbitrary<ExceptionFreeStack<Integer>> arbStack() {
-//        arbitrary(genStackImperative())
-        arbitrary(genStackRecursive())
-    }
-
     @Test
     @TypeChecked(TypeCheckingMode.SKIP)
     void testPush() {
-        showAll new PropertyConfig(
-            map: DEFAULT_MAP + [(ExceptionFreeStack.class): arbStack()],
-            function: { ExceptionFreeStack<Integer> s, Integer i ->
-                println "pushing $i onto ${s.toString()}"
-                s.push(i)
-                s.top() == i
-            }
-        )
+        [genStackRecursive(), genStackImperative()].collect { g ->
+            showAll new PropertyConfig(
+                map: DEFAULT_MAP + [(ExceptionFreeStack.class): arbitrary(g)],
+                function: { ExceptionFreeStack<Integer> s, Integer i ->
+//                    println "pushing $i onto ${s.toString()}"
+                    s.push(i)
+                    s.top() == i
+                }
+            )
+        }
     }
 
 }

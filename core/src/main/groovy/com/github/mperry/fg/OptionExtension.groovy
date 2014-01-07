81765142b78eaeaea1ff63034669081a772a2743
Mark Perry <maperry78@yahoo.com.au>
2014-01-07 13:13:20 +1000

    Typecheck Option extension classes as much as possible

--- core/src/main/groovy/com/github/mperry/fg/OptionExtension.groovy
+++ core/src/main/groovy/com/github/mperry/fg/OptionExtension.groovy
@@ -3,6 +3,7 @@ package com.github.mperry.fg
 import fj.data.Option
 import fj.F
 import groovy.transform.TypeChecked
+import groovy.transform.TypeCheckingMode
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,37 +12,34 @@ import groovy.transform.TypeChecked
  * Time: 12:28 PM
  * To change this template use File | Settings | File Templates.
  */
+@TypeChecked
 class OptionExtension {
 
 	/**
 	 * (M a) -> (a -> M b) -> (M b)
 	 */
-	@TypeChecked
 	public static <A, B> Option<B> bind(Option<A> option, Closure<Option<B>> f) {
 		option.bind(f as F)
 	}
 
-//	@TypeChecked
 	static <A, B> Option<B> collectMany(Option<A> option, Closure<Option<B>> f) {
-		option.bind(f)
+        bind(option, f)
 	}
 
-	@TypeChecked
 	public static <A, B> Option<B> map(Option<A> option, Closure<B> f) {
 		option.map(f as F)
 	}
 
 	static <A, B> Option<B> collect(Option<A> option, Closure<B> f) {
-		option.map(f)
+        map(option, f)
 	}
 
-	@TypeChecked
 	static <A> Option<A> filter(Option<A> option, Closure<Boolean> f) {
 		option.filter(f as F)
 	}
 
 	static <A> Option<A> findAll(Option<A> option, Closure<Boolean> f) {
-		option.filter(f)
+        filter(option, f)
 	}
 
 }

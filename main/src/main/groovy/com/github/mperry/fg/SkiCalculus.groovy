290bd91fe250ffa02fd1c1385b0855cc92fd77ea
Mark Perry <maperry78@yahoo.com.au>
2014-01-15 00:30:07 +1000

    Started on the SKI calculus

---
+++ main/src/main/groovy/com/github/mperry/fg/SkiCalculus.groovy
@@ -0,0 +1,20 @@
+package com.github.mperry.fg
+
+/**
+ * Created by MarkPerry on 14/01/14.
+ */
+class SkiCalculus {
+
+    void I() {
+
+    }
+
+    void K() {
+
+    }
+
+    void S() {
+
+    }
+    
+}

34db2d644927f539ccf09df64b7974a49639c7cc
Mark Perry <maperry78@yahoo.com.au>
2014-01-20 12:01:20 +1000

    Added more stream extensions for monads

---
+++ main/src/main/groovy/com/github/mperry/fg/StreamExtension2.groovy
@@ -0,0 +1,12 @@
+package com.github.mperry.fg
+
+import groovy.transform.TypeChecked
+
+/**
+ * Created by MarkPerry on 20/01/14.
+ */
+@TypeChecked
+class StreamExtension2 {
+
+
+}
---
+++ main/src/main/groovy/com/github/mperry/fg/StreamStaticExtension2.groovy
@@ -0,0 +1,11 @@
+package com.github.mperry.fg
+
+import groovy.transform.TypeChecked
+
+/**
+ * Created by MarkPerry on 20/01/14.
+ */
+@TypeChecked
+class StreamStaticExtension2 {
+
+}

00c98b0d3f4cf0926a7d43c83556bbcefae44ef8
Mark Perry <maperry78@yahoo.com.au>
2014-04-14 11:31:20 +1000

    Added SetMonad tests for the extension module

--- test-extensions/src/test/groovy/com/github/mperry/fg/SetMonadExtensionTEst.groovy
+++ test-extensions/src/test/groovy/com/github/mperry/fg/SetMonadExtensionTEst.groovy
@@ -26,74 +26,100 @@ class SetMonadExtensionTest {
     @Test
     void ap() {
         def f = { Integer i -> i + 1} as F
-        assertTrue(monad().ap([1, 1, 2].toSet(), [f, f].toSet()) == [2, 3].toSet())
+        def source = [1, 1, 2].toSet()
+        def fs = [f, f].toSet()
+        def expected = [2, 3].toSet()
+        assertTrue(source.ap(fs) == expected)
+        assertTrue(monad().ap(source, fs) == expected)
     }
 
     @Test
     void filterM() {
         def f = { Integer i -> [i > 0].toSet()} as F
-        def actual = monad().filterM([-1, 2, 0, 1, -1], f)
+        def source = [-1, 2, 0, 1, -1]
+        def actual = monad().filterM(source, f)
 //        println actual
-        assertTrue(actual == [[2, 1]].toSet())
+        def expected = [[2, 1]].toSet()
+        assertTrue(monad().filterM(source, f) == expected)
+        assertTrue(Set.filterM(source, f) == expected)
     }
 
     @Test
     void fmap() {
-        def actual = monad().fmap(Integers.negate, [2, 3].toSet())
-        assertTrue(actual == [-2, -3].toSet())
+        def f = Integers.negate
+        def source = [2, 3].toSet()
+        def expected = [-2, -3].toSet()
+        assertTrue(monad().fmap(f, source) == expected)
+        assertTrue(source.fmap(f) == expected)
     }
 
     @Test
     void foldM() {
         def f = { Integer i, Integer j -> [i + j].toSet() } as F2
-        def actual = monad().foldM([2, 4, 5], 1, f)
+        def init = 1
+        def source = [2, 4, 5]
 //        println actual
-        assertTrue(actual == [12].toSet())
+        def expected = [12].toSet()
+        assertTrue(monad().foldM(source, init, f) == expected)
+        assertTrue(Set.foldM(source, init, f) == expected)
 
     }
 
     @Test
     void join() {
-        def actual = monad().join([[2, 4].toSet(), [2, 6].toSet()].toSet())
+        def s1 = [[2, 4].toSet(), [2, 6].toSet()].toSet()
 //        println actual
-        assertTrue(actual == [2, 4, 6].toSet())
+        def expected = [2, 4, 6].toSet()
+        assertTrue(monad().join(s1) == expected)
+        assertTrue(Set.join(s1) == expected)
     }
 
     @Test
     void liftM() {
         def f = Integers.negate
-        assertTrue(monad().liftM([2, 4].toSet(), f) == [-2, -4].toSet())
-
+        def expected = [-2, -4].toSet()
+        def source = [2, 4].toSet()
+        assertTrue(monad().liftM(source, f) == expected)
+        assertTrue(source.liftM(f) == expected)
     }
 
     @Test
     void liftM2() {
         def f = uncurryF2(Integers.multiply)
-        assertTrue(monad().liftM2([2, 3].toSet(), [4, 5].toSet(), f) == [8, 10, 12, 15].toSet())
+        def expected = [8, 10, 12, 15].toSet()
+        def s1 = [2, 3].toSet()
+        def s2 = [4, 5].toSet()
+        assertTrue(monad().liftM2(s1, s2, f) == expected)
+        assertTrue(s1.liftM2(s2, f) == expected)
     }
 
     @Test
     void sequence() {
-        def actual = monad().sequence([[1, 2].toSet(), [1, 3].toSet()])
+        def source = [[1, 2].toSet(), [1, 3].toSet()]
 //        println actual
-        assertTrue(actual == [[1, 1], [1, 3], [2, 1], [2, 3]].toSet())
+        def expected = [[1, 1], [1, 3], [2, 1], [2, 3]].toSet()
+        assertTrue(monad().sequence(source) == expected)
+        assertTrue(Set.sequence(source) == expected)
     }
 
     @Test
     void replicateM() {
-        def actual = monad().replicateM(3, [1, 2].toSet())
+        def n = 3
+        def s = [1, 2].toSet()
 //        println actual
         def expected = [[1, 1, 1], [2, 2, 1], [1, 2, 2], [2, 1, 1], [1, 1, 2], [2, 2, 2], [2, 1, 2], [1, 2, 1]].toSet()
-        assertTrue(actual == expected)
+        assertTrue(monad().replicateM(n, s) == expected)
+        assertTrue(s.replicateM(n) == expected)
     }
 
     @Test
     void traverse() {
         def f = { Integer i -> (i..i + 1).toSet() } as F
-        def actual = monad().traverse([2, 4], f)
+        def s = [2, 4]
 //        println actual
         def expected = [[3, 4], [2, 4], [3, 5], [2, 5]].toSet()
-        assertTrue(actual == expected)
+        assertTrue(monad().traverse(s, f) == expected)
+        assertTrue(Set.traverse(s, f) == expected)
     }
 
 }

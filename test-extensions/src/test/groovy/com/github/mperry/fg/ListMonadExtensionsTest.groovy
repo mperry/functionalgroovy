fdb84fe8849e3819fee051c5de8524b07259d6f9
Mark Perry <maperry78@yahoo.com.au>
2014-04-14 10:44:55 +1000

    Cleaned up testing of monad and extension

--- test-extensions/src/test/groovy/com/github/mperry/fg/ListMonadExtensionsTest.groovy
+++ test-extensions/src/test/groovy/com/github/mperry/fg/ListMonadExtensionsTest.groovy
@@ -24,53 +24,69 @@ class ListMonadExtensionsTest {
 
     @Test
     void join() {
-        assertTrue(List.join([[1, 2], [3, 4], []]) == [1, 2, 3, 4])
-        assertTrue(monad().join([[1, 2], [3, 4], []]) == [1, 2, 3, 4])
+        def source = [[1, 2], [3, 4], []]
+        def expected = [1, 2, 3, 4]
+        assertTrue(List.join(source) == expected)
+        assertTrue(monad().join(source) == expected)
     }
 
     @Test
     void map() {
-        assertTrue([1, 2, 3].map({ Integer i -> i * 2} as F) == [2, 4, 6])
-        assertTrue(monad().map([1, 2, 3], { Integer i -> i * 2} as F) == [2, 4, 6])
+        def f = { Integer i -> i * 2} as F
+        def source = [1, 2, 3]
+        def expected = [2, 4, 6]
+        assertTrue(source.map(f) == expected)
+        assertTrue(monad().map(source, f) == expected)
     }
 
     @Test
     void map2() {
         def f = { Integer i, Integer j -> i * j } as F2
-        def list = [1, 2].map2([3, 4], f)
-        assertTrue(list == [3, 4, 6, 8])
-        assertTrue(monad().map2([1, 2], [3, 4], f) == [3, 4, 6, 8])
+        def list1 = [1, 2]
+        def list2 = [3, 4]
+        def expected = [3, 4, 6, 8]
+        assertTrue(list1.map2(list2, f) == expected)
+        assertTrue(monad().map2(list1, list2, f) == expected)
     }
 
     @Test
     @TypeChecked(TypeCheckingMode.SKIP)
     void to() {
         def s = "a"
-        def list = [1, 2].to(s)
-        assertTrue([s, s] == list)
-        assertTrue(monad().to([1, 2], s) == [s, s])
+        def source = [1, 2]
+        def expected = [s, s]
+        assertTrue(source.to(s) == expected)
+        assertTrue(monad().to(source, s) == expected)
     }
 
     @Test
     void skip() {
-        assertTrue([1, 2].skip() == [unit(), unit()])
-        assertTrue(monad().skip([1, 2]) == [unit(), unit()])
+        def expected = [unit(), unit()]
+        def source = [1, 2]
+        assertTrue(source.skip() == expected)
+        assertTrue(monad().skip(source) == expected)
     }
 
     @Test
     void foldM() {
         def f = { Integer acc, Integer i -> [acc + i] } as F2
-        def list = List.foldM(1.to(3), 0, f)
-        assertTrue(list == [6])
-        assertTrue(monad().foldM(1.to(3), 0, f) == [6])
+        def source = 1.to(3)
+        def init = 0
+        def list = List.foldM(source, init, f)
+        def expected = [6]
+        assertTrue(list == expected)
+        assertTrue(monad().foldM(source, init, f) == expected)
     }
 
     @Test
     void foldM_() {
         def f = { Integer acc, Integer i -> [acc + i] } as F2
-        def list = List.foldM_(1.to(3), 0, f)
-        assertTrue(list == [unit()])
-        assertTrue(monad().foldM_(1.to(3), 0, f) == [unit()])
+        def source = 1.to(3)
+        def init = 0
+        def list = List.foldM_(source, init, f)
+        def expected = [unit()]
+        assertTrue(list == expected)
+        assertTrue(monad().foldM_(source, init, f) == expected)
     }
 
     @Test
@@ -78,28 +94,30 @@ class ListMonadExtensionsTest {
         def source = [[1, 2], [3, 4, 5]]
         def list = List.sequence(source)
 //        println list
-        def haskell = [[1,3],[1,4],[1,5],[2,3],[2,4],[2,5]]
-        assertTrue(list == haskell)
-        assertTrue(monad().sequence(source) == haskell)
+        def expected = [[1,3],[1,4],[1,5],[2,3],[2,4],[2,5]]
+        assertTrue(list == expected)
+        assertTrue(monad().sequence(source) == expected)
     }
 
     @Test
     void traverse() {
         def f = { Integer i -> 1.to(i).toJList()} as F
-        def list = [1, 2, 3].traverse(f)
+        def source = [1, 2, 3]
 //        println list
-        def haskell = [[1,1,1],[1,1,2],[1,1,3],[1,2,1],[1,2,2],[1,2,3]]
-        assertTrue(list == haskell)
-        assertTrue(monad().traverse([1, 2, 3], f) == haskell)
+        def expected = [[1,1,1],[1,1,2],[1,1,3],[1,2,1],[1,2,2],[1,2,3]]
+        assertTrue(source.traverse(f) == expected)
+        assertTrue(monad().traverse(source, f) == expected)
     }
 
     @Test
     void replicateM() {
-        def list = [1, 2].replicateM(3)
+        def source = [1, 2]
+        def n = 3
+        def list = source.replicateM(n)
 //        println list
-        def haskell = [[1,1,1],[1,1,2],[1,2,1],[1,2,2],[2,1,1],[2,1,2],[2,2,1],[2,2,2]]
-        assertTrue(list == haskell)
-        assertTrue(monad().replicateM(3, [1, 2]) == haskell)
+        def expected = [[1,1,1],[1,1,2],[1,2,1],[1,2,2],[2,1,1],[2,1,2],[2,2,1],[2,2,2]]
+        assertTrue(list == expected)
+        assertTrue(monad().replicateM(n, source) == expected)
     }
 
     @Test
@@ -111,10 +129,10 @@ class ListMonadExtensionsTest {
             } as F)
         } as F
         def func = List.compose(f, g)
-        def list = func.f(3)
+        def v = 3
         def expected = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6].collect { it.toString() }
-        assertTrue(list == expected)
-        assertTrue(monad().compose(f, g).f(3) == expected)
+        assertTrue(List.compose(f, g).f(v) == expected)
+        assertTrue(monad().compose(f, g).f(v) == expected)
     }
 
     @Test
@@ -122,10 +140,11 @@ class ListMonadExtensionsTest {
         def f = { Integer i ->
             [i > 0]
         } as F
-        def actual = List.filterM([2, 1, 0, -1], f)
+        def source = [2, 1, 0, -1]
 //        println actual
-        assertTrue(actual == [[2, 1]])
-        assertTrue(monad().filterM([2, 1, 0, -1], f) == [[2, 1]])
+        def expected = [[2, 1]]
+        assertTrue(List.filterM(source, f) == expected)
+        assertTrue(monad().filterM(source, f) == expected)
     }
 
     @Test
@@ -142,31 +161,33 @@ class ListMonadExtensionsTest {
     void liftM() {
         def source = [1, 2, 3]
         def f = { Integer i -> i * 2 } as F
-        def actual = source.liftM(f)
         def expected = source.map(f)
 //        println actual
-        assertTrue(expected == actual)
+        assertTrue(source.liftM(f) == expected)
         assertTrue(monad().liftM(source, f) == expected)
     }
 
     @Test
     void liftM2() {
         def f = { Integer i, Integer j -> i + j } as F2
-        def actual = [0, 1].liftM2([0, 2], f)
+        def list1 = [0, 1]
+        def list2 = [0, 2]
+        def expected = [0, 2, 1, 3]
 //        println actual
-        assertTrue(actual == [0, 2, 1, 3])
-        assertTrue(monad().liftM2([0, 1], [0, 2], f) == [0, 2, 1, 3])
+        assertTrue(list1.liftM2(list2, f) == expected)
+        assertTrue(monad().liftM2(list1, list2, f) == expected)
     }
 
     @Test
     void liftM3() {
         // TODO: continue from here
         def f = {Integer i, Integer j, Integer k -> i + j + k } as F3
-        def actual = [0, 1].liftM3([0, 2], [0, 4], f)
+        def list1 = [0, 1]
+        def list2 = [0, 2]
+        def list3 = [0, 4]
         def expected = [0, 4, 2, 6, 1, 5, 3, 7]
-
-        assertTrue(actual == expected)
-        assertTrue(monad().liftM3([0, 1], [0, 2], [0, 4], f) == expected)
+        assertTrue(list1.liftM3(list2, list3, f) == expected)
+        assertTrue(monad().liftM3(list1, list2, list3, f) == expected)
     }
 
     @Test
@@ -177,11 +198,11 @@ class ListMonadExtensionsTest {
             def f = { Integer j -> j * (i + 2)}
             f as F
         }.toJavaList()
-        def actual = 1.to(4).toJavaList().ap(fs)
+        def list = 1.to(4).toJavaList()
         def expected = [5,10,15,20,6,12,18,24,7,14,21,28]
 //        println actual
-        assertTrue(actual == expected)
-        assertTrue(monad().ap(1.to(4).toJavaList(), fs) == expected)
+        assertTrue(list.ap(fs) == expected)
+        assertTrue(monad().ap(list, fs) == expected)
     }
 
     @Test

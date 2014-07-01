04b063ff8edfe729c1701f18e9399393f82db51a
Mark Perry <mperry@onetest.com.au>
2014-07-01 16:50:09 +1000

    Added type checking on collection extension

--- core/src/main/groovy/com/github/mperry/fg/CollectionExtension.groovy
+++ core/src/main/groovy/com/github/mperry/fg/CollectionExtension.groovy
@@ -15,74 +15,36 @@ import groovy.transform.TypeCheckingMode
 @TypeChecked
 class CollectionExtension {
 
-    @TypeChecked(TypeCheckingMode.SKIP)
 	static <A, B> List<B> map(Collection<A> collection, Closure<B> f) {
 		collection.collect(f)
 	}
 
-    @TypeChecked(TypeCheckingMode.SKIP)
     static <A, B> List<B> map(Collection<A> collection, F<A, B> f) {
-        map(collection, f.toClosure())
+        collection.collect(FExtension.toClosure(f))
     }
 
-    @TypeChecked(TypeCheckingMode.SKIP)
-	static <A> Collection<A> filter(Collection<A> collection, Closure<Boolean> f) {
-		collection.findAll(f)
-	}
-
-    @TypeChecked(TypeCheckingMode.SKIP)
     static <A> Collection<A> filter(Collection<A> collection, F<A, Boolean> f) {
-        filter(collection, f.toClosure())
+        collection.findAll(FExtension.toClosure(f))
     }
 
-    @TypeChecked(TypeCheckingMode.SKIP)
-	static <A, B> B fold(Collection<A> collection, B initial, Closure<B> f) {
-        (B) collection.inject(initial, f)
-	}
-
-    @TypeChecked(TypeCheckingMode.SKIP)
     static <A, B> B fold(Collection<A> collection, B initial, F<A, B> f) {
-        fold(collection, f.toClosure())
+        collection.inject(initial, FExtension.toClosure(f))
     }
 
-    @TypeChecked(TypeCheckingMode.SKIP)
-    public static <A, B> List<B> flatMap(Collection<A> c, Closure<List<B>> f) {
-        c.collectMany(f)
+    static <A, B> List<B> flatMap(Collection<A> c, F<A, List<B>> f) {
+        c.collectMany(FExtension.toClosure(f))
     }
 
-    @TypeChecked(TypeCheckingMode.SKIP)
-    public static <A, B> List<B> flatMap(Collection<A> c, F<A, List<B>> f) {
-        flatMap(c, f.toClosure())
-    }
-
-    @TypeChecked(TypeCheckingMode.SKIP)
-	public static <A> Boolean exists(Collection<A> c, Closure<Boolean> f) {
-        exists(c, f as F)
-	}
-
-    @TypeChecked(TypeCheckingMode.SKIP)
-    public static <A> Boolean exists(Collection<A> c, F<A, Boolean> f) {
+    static <A> Boolean exists(Collection<A> c, F<A, Boolean> f) {
         findFirst(c, f).isSome()
     }
 
-    @TypeChecked(TypeCheckingMode.SKIP)
-    public static <A> Boolean forAll(Collection<A> c, F<A, Boolean> f) {
+    static <A> Boolean forAll(Collection<A> c, F<A, Boolean> f) {
         !exists(c, { A a -> f.f(a) == false } as F)
     }
 
-    @TypeChecked(TypeCheckingMode.SKIP)
-    public static <A> Boolean forAll(Collection<A> c, Closure<Boolean> f) {
-        forAll(c, f as F)
-	}
-
-    @TypeChecked(TypeCheckingMode.SKIP)
-	public static <A> Option<A> findFirst(Collection<A> c, Closure<Boolean> f) {
-		Option.fromNull(c.find(f))
-	}
-
-    @TypeChecked(TypeCheckingMode.SKIP)
-    public static <A> Option<A> findFirst(Collection<A> c, F<A, Boolean> f) {
-        findFirst(c, f.toClosure())
+    static <A> Option<A> findFirst(Collection<A> c, F<A, Boolean> f) {
+        Option.fromNull(c.find(FExtension.toClosure(f)))
     }
 
 }

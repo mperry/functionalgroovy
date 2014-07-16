b8e102cd5e95f329f25797ab4dffc53619bec1b8
Mark Perry <maperry78@yahoo.com.au>
2014-07-17 00:06:45 +1000

    Started on IO monad test

--- test-extensions/src/test/groovy/com/github/mperry/fg/IOMonadTest.groovy
+++ test-extensions/src/test/groovy/com/github/mperry/fg/IOMonadTest.groovy
@@ -1,7 +1,29 @@
 package com.github.mperry.fg
 
+import com.github.mperry.fg.typeclass.concrete.IOMonad
+import fj.F
+import fj.F2
+import fj.function.Integers
+import groovy.transform.TypeChecked
+import groovy.transform.TypeCheckingMode
+import org.junit.Test
+
+import static fj.Function.uncurryF2
+import static fj.data.Option.none
+import static fj.data.Option.some
+import static junit.framework.Assert.assertTrue
+
 /**
  * Created by MarkPerry on 15/07/2014.
  */
+@TypeChecked
+//@TypeChecked(TypeCheckingMode.SKIP)
 class IOMonadTest {
+
+
+    IOMonad monad() {
+        new IOMonad()
+    }
+
+
 }
--- typeclass/src/main/groovy/com/github/mperry/fg/typeclass/concrete/IOMonad.groovy
+++ typeclass/src/main/groovy/com/github/mperry/fg/typeclass/concrete/IOMonad.groovy
@@ -1,7 +1,25 @@
 package com.github.mperry.fg.typeclass.concrete
 
+import com.github.mperry.fg.typeclass.Monad
+import fj.F
+import fj.data.IO
+import fj.data.IOFunctions
+import groovy.transform.TypeChecked
+
 /**
  * Created by MarkPerry on 15/07/2014.
  */
-class IOMonad {
+@TypeChecked
+class IOMonad extends Monad<IO> {
+
+    @Override
+    def <A> IO<A> unit(A a) {
+        { -> a } as IO
+    }
+
+    @Override
+    def <A, B> IO<B> flatMap(IO<A> io, F<A, IO<B>> f) {
+        IOFunctions.bind(io, f)
+    }
+
 }
